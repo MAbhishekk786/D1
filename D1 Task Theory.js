0bdbada6a43942fa7fb4a9e8dae488b512522d6b
MAbhishekk786 <94685983+MAbhishekk786@users.noreply.github.com>
2022-02-16 12:31:01 +0530

    Add files via upload
    D1 Theory Task

---
+++ D1 Task Theory.js
@@ -0,0 +1,15 @@
+//Difference Between HTTP1 & HTTP2
+
+//While HTTP1 loads multiple requests for every TCP connection, 
+//HTTP2 avoids network delay by multiplexing. 
+//HTTP is a network delay sensitive protocol in the sense that if 
+//there is less network delay then the page loads faster. 
+//However, an impressive increase in network bandwidth has negatively impacted HTTP2.
+
+
+//Objects & Internal Representation
+
+//Objectsare the representation of real-world entities in 
+//any language representing things by defining its properties along with their values. 
+//In Javascript, objects may be defined as 
+//an unordered collection of related data, of primitive or reference types, in the form of “key: value” pairs.
